dc42b87987133f5da60cc9c9c9981ce3ef55b3db
VitalyKliauzo <vitaly.kliauzo@gmail.com>
2016-06-27 15:54:05 +0300

    windows environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -11,31 +11,13 @@ const webpackConfig = {
 
     devtool: 'source-map',
 
-    resolve: {
-        //Where modules
-        root: path.resolve(__dirname, 'app'),
-        modulesDirectories: [ 'node_modules' ],
-        extensions: [ '', '.js' ]
-    },
-
-    resolveLoader: {
-        //where loader-modules
-        modulesDirectories: [ 'node_modules' ],
-        moduleTemplates: [ '*-loader', '*' ],
-        extensions: [ '', '.js' ]
-    },
-
     module: {
         loaders: [
             {
-                test: /\.js$/,
-                loader: 'babel',
-                include: path.resolve(__dirname, 'app'),
-                exclude: /node_modules/,
-                query: {
-                    presets: [ 'es2015', 'react' ]
-                }
-            }
+		      test: /\.js?$/,
+		      exclude: /(node_modules)/,
+		      loaders: ['react-hot', 'babel'],
+ 	        },
         ]
     }
 };
